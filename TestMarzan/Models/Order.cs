362cfaa80faf2920882d3d81705d04d0dbbf6f6d
CristianGomezCTC <83828202+CristianGomezCTC@users.noreply.github.com>
2022-10-12 18:30:16 -0400

    Initial Commit

---
+++ TestMarzan/Models/Order.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestMarzan.Models
+{
+    public class Order
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int OrderNumber { get; set; }
+        [Required]
+        public int CostumerId { get; set; }
+        public Customer Customer { get; set; }
+        [Required]
+        public int ProductCount { get; set; }
+        [Required]
+        public string Products { get; set; }
+        [Required]
+        public int TotalPrice { get; set; }
+    }
+}
